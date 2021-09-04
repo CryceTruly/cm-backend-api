ba5461a77e494d84371eb3b1c12e5aada3b6213f
crycetruly <crycetruly@gmail.com>
2021-09-04 20:12:34 +0300

    Add allowed hosts

--- cmbackend/settings.py
+++ cmbackend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://cmbackendapi.herokuapp.com"]
+ALLOWED_HOSTS = ["cmbackendapi.herokuapp.com"]
 
 
 # Application definition
