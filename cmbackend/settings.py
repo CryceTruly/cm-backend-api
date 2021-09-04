d85655b565cc9466877ed120d663a0a23d446542
crycetruly <crycetruly@gmail.com>
2021-09-04 20:19:54 +0300

    Change debug

--- cmbackend/settings.py
+++ cmbackend/settings.py
@@ -33,10 +33,9 @@ cloudinary.config(
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["cmbackendapi.herokuapp.com"]
-
 
 # Application definition
 
