39a414b1dd1ccad3cea39456a5abdcef2efc8888
crycetruly <crycetruly@gmail.com>
2021-09-04 20:14:28 +0300

    Add allowed hosts

--- cmbackend/settings.py
+++ cmbackend/settings.py
@@ -33,7 +33,7 @@ cloudinary.config(
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["cmbackendapi.herokuapp.com"]
 
